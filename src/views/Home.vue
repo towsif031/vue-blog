c9b3662fef5a330ccad323d3f7caa0600264cb3f
Towsif Ahmed Omi <towsif031@gmail.com>
2021-09-17 23:29:00 +0600

    template refs with composition api.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,29 @@
 <template>
 	<div class="home">
-		<p>{{ name }} is the {{ role }} of Deviluke.</p>
+		<h1>Home</h1>
+		<p ref="p">{{ name }} is the {{ role }} of Deviluke.</p>
 		<button @click="handleClick">Click</button>
 	</div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity';
 export default {
 	name: 'Home',
 	setup() {
+		const p = ref(null);
+		console.log(p, p.value);
+
 		let name = 'Lala';
 		let role = 'Princess';
 
 		const handleClick = () => {
-			console.log('button clicked.');
+			console.log(p, p.value);
+			p.value.classList.add('test');
+			p.value.textContent = 'Hello, Yukki!';
 		};
 
-		return { name, role, handleClick };
+		return { name, role, handleClick, p };
 	}
 };
 </script>
