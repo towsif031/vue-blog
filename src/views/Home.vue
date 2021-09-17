ddf7ca0fd087f8c5acb80e0cc76c667edb148826
Towsif Ahmed Omi <towsif031@gmail.com>
2021-09-17 22:55:48 +0600

    composition api basic.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,17 @@
 <template>
-	<div class="home"></div>
+	<div class="home">
+		<p>{{ name }} is the {{ role }} of Deviluke.</p>
+	</div>
 </template>
 
 <script>
 export default {
-	name: 'Home'
+	name: 'Home',
+	setup() {
+		let name = 'Lala';
+		let role = 'Princess';
+
+		return { name, role };
+	}
 };
 </script>
