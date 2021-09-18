deefeb8ec55b57f920fc472c56a6bb87f5a41219
Towsif Ahmed Omi <towsif031@gmail.com>
2021-09-18 19:52:50 +0600

    post details page.

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,9 +1,40 @@
 <template>
-	details
+	<div v-if="error">
+		{{ error }}
+	</div>
+	<div v-if="post" class="post">
+		<h3>{{ post.title }}</h3>
+		<p class="pre">{{ post.body }}</p>
+	</div>
+	<div v-else>Loading...</div>
 </template>
 
 <script>
-export default {};
+import getPost from '../composables/getPost';
+
+export default {
+	props: ['id'],
+	setup(props) {
+		const { post, error, load } = getPost(props.id);
+
+		load();
+
+		return { post, error };
+	}
+};
 </script>
 
-<style></style>
+<style>
+.post {
+	max-width: 1200px;
+	margin: 0 auto;
+}
+.post p {
+	color: #444;
+	line-height: 1.5em;
+	margin-top: 40px;
+}
+.pre {
+	white-space: pre-wrap;
+}
+</style>
