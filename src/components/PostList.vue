4e38c098973e8544600114297bdc2210b1d49324
Towsif Ahmed Omi <towsif031@gmail.com>
2021-09-18 17:41:46 +0600

    life cycle hooks using composition api.

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -5,13 +5,16 @@
 </template>
 
 <script>
+import { onMounted, onUnmounted, onUpdated } from '@vue/runtime-core';
 import SinglePost from './SinglePost.vue';
 
 export default {
 	props: ['posts'],
 	components: { SinglePost },
 	setup(props) {
-		console.log(props.posts);
+		onMounted(() => console.log('component mounted'));
+		onUnmounted(() => console.log('component unmounted'));
+		onUpdated(() => console.log('component updated'));
 	}
 };
 </script>
